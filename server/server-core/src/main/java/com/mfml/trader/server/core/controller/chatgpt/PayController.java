7085b2ef65968377d0bf70e5194519505e43c0df
caozhou.lwj <caozhou.lwj@alibaba-inc.com>
2023-03-31 16:25:28 +0800

    chat

--- server/server-core/src/main/java/com/mfml/trader/server/core/controller/chatgpt/PayController.java
+++ server/server-core/src/main/java/com/mfml/trader/server/core/controller/chatgpt/PayController.java
@@ -8,6 +8,7 @@ import com.mfml.trader.server.core.pay.ro.SecretProduceRo;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
@@ -27,14 +28,14 @@ public class PayController {
     @ApiOperation(value = "秘钥验证", notes = "秘钥验证", tags = {"Pay"})
     @PostMapping(value = "payValidation")
     @ResponseBody
-    public Result<Integer> payValidation(PayValidationRo ro) {
+    public Result<Integer> payValidation(@RequestBody PayValidationRo ro) {
         return payFacade.payValidation(ro);
     }
 
     @ApiOperation(value = "秘钥生成", notes = "秘钥生成", tags = {"Pay"})
     @PostMapping(value = "secretProduce")
     @ResponseBody
-    public Result<List<String>> secretProduce(SecretProduceRo ro) {
+    public Result<List<String>> secretProduce(@RequestBody SecretProduceRo ro) {
         return payFacade.secretProduce(ro);
     }
 }
