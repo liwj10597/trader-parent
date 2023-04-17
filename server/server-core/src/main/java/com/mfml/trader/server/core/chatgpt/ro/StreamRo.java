6bc0d0b8e616a5614e3539ff4d8c6f05cf6de2b8
caozhou.lwj <caozhou.lwj@alibaba-inc.com>
2023-04-17 16:53:20 +0800

    1

---
+++ server/server-core/src/main/java/com/mfml/trader/server/core/chatgpt/ro/StreamRo.java
@@ -0,0 +1,17 @@
+package com.mfml.trader.server.core.chatgpt.ro;
+
+import com.mfml.trader.common.core.model.base.ToString;
+import lombok.Data;
+
+/**
+ * @author caozhou
+ * @date 2023-04-17 16:45
+ */
+@Data
+public class StreamRo extends ToString {
+    private String content;
+    private String prompt;
+    private Integer maxTokens;
+    private Double temperature;
+    private String uid;
+}
